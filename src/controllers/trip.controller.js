603908e12c2dc2f8df16634d9b927730b936da60
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-09-16 21:31:15 +0530

    bug fix

--- src/controllers/trip.controller.js
+++ src/controllers/trip.controller.js
@@ -1,18 +1,18 @@
 const Trip = require('../schemas/trip.schema');
-const HttpError = require('../models/http-error');
+const HttpError = require('../models/http-error.model');
 
 exports.saveTrip = async (req, res, next) => {
     if (!req.body) {
         res.send(new HttpError('Invalid request body', 400));
     }
     const { source, destination, route, bus, noOfPassengers } = req.body;
-    const trip = new Trip(
+    const trip = new Trip({
         source,
         destination,
         route,
         bus,
         noOfPassengers
-    );
+    });
     try {
         const savedTrip = await trip.save();
         res.status(200).send({ data: savedTrip });
